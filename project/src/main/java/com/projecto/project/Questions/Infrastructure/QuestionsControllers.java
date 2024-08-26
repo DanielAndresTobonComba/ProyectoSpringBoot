da115b91b131c904bc71b46698e847812ee6b84f
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-08-26 16:53:22 -0500

    listo el buscar id para surveyJson

--- project/src/main/java/com/projecto/project/Questions/Infrastructure/QuestionsControllers.java
+++ project/src/main/java/com/projecto/project/Questions/Infrastructure/QuestionsControllers.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RestController("/")
-@RequestMapping()
+@RestController()
+@RequestMapping("/")
 public class QuestionsControllers {
 
     @Autowired
