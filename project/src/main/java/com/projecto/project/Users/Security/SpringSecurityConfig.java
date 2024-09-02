711af2a1942763bde767c156d755f47c7bed9ad7
carlosjhoan <carlosjhoanaguilar@gmail.com>
2024-09-02 17:54:31 -0500

    Perdiendo el tiempo MIME type del javascript

--- project/src/main/java/com/projecto/project/Users/Security/SpringSecurityConfig.java
+++ project/src/main/java/com/projecto/project/Users/Security/SpringSecurityConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +18,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+        "classpath:/META-INF/resources/", "classpath:/resources/",
+        "classpath:/static/", "classpath:/public/" };
 
     @Override
     public void addViewControllers(@SuppressWarnings("null") ViewControllerRegistry registry) {
@@ -32,19 +38,33 @@ public class SpringSecurityConfig implements WebMvcConfigurer {
                 .allowedHeaders("");
     }
 
+    
+
+    @Override
+    public void addResourceHandlers(@SuppressWarnings("null") ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/index").permitAll()
+                .requestMatchers("/home/camper/Documentos/carlosJhoanAguilarGalvis/SpringBoot/ProyectoSpringBoot/project/src/main/resources/static/surveyGenerator.js").permitAll()
+                .requestMatchers("/survey").permitAll()
                 .requestMatchers("/api/categories").permitAll()
                 .requestMatchers("/api/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/verify").permitAll()
                 .anyRequest().authenticated())
+                
                 // .formLogin((form) -> form
                 //     .loginPage("/index").permitAll())
                 .csrf(config -> config.disable())
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                
                 .formLogin(form -> form
                     .loginPage("/index").permitAll())   
                 .build();
