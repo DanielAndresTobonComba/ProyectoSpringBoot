a2853f3d74f809c6b2307288f0ec1c28f80f1426
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-09-04 02:31:46 -0500

    Funcionalidad de añadir opción ya funcional

--- project/src/main/resources/static/surveyGenerator.js
+++ project/src/main/resources/static/surveyGenerator.js
@@ -1,14 +1,8 @@
 let jsonList;
-// let lastidButtonActived = "";
+
+let optionsList = ["Única Respuesta", "Respuesta Múltiple", "Respuesta Abierta"];
 
 document.body.setAttribute("onload", "onLoadCreate()");
-document.getElementById("categories").setAttribute("onchange", "seeCategory()");
-
-function seeCategory() {
-    let categoryElement = document.getElementById("categories");
-    console.log(categoryElement.value);
-}
-
 
 
 function onLoadCreate() {
@@ -46,8 +40,6 @@ function onLoadCreate() {
 
 
 
-
-
 function createQuestion() {
     let  count = 1;
     const myCustomListener = function(e) {
@@ -63,6 +55,7 @@ function createQuestion() {
             let idNewHeaderQuestion = "header-question-";
             let idNewTextQuestion = "text-question-";
             let idNewSelectOptionType = "select-question-";
+            let idNewResponseOption = "option-question-";
             let secondPartIdContent = className.split(" ")[2];
             let idContentSection = firtPartIdContent.concat(secondPartIdContent)
             let divIdContentSection = document.getElementById(idContentSection);
@@ -73,16 +66,34 @@ function createQuestion() {
             idNewHeaderQuestion = idNewHeaderQuestion + extenderId;
             idNewTextQuestion = idNewTextQuestion + extenderId;
             idNewSelectOptionType = idNewSelectOptionType  + extenderId;
+            idNewResponseOption = idNewResponseOption + extenderId;
             console.log(idNewQuestionContent);
             let newQuestionContent = document.createElement("div");
             newQuestionContent.id= idNewQuestionContent;
             newQuestionContent.className="w-100";
-            newQuestionContent.innerHTML= `<div id="${idNewHeaderQuestion}" class="d-flex justify-content-around centrado-vertical header-question"><input class="question ${idNewTextQuestion} w-50 border border-0 border-bottom p-2 bg-body-tertiary"><button type="button" class="btn btn-outline-success">Añadir Opción</button><select id = "${idNewSelectOptionType}" class="form-select w-25 shadow p-3 mb-5 bg-body-tertiary rounded" aria-label="Default select example"><option value="" selected>Tipo de respuesta</option></select><button type="button" class="btn-close close-button" aria-label="Close"></button></div>`;
-            // newQuestion.value="Nueva pregunta";
-            // newQuestion.className= secondPartIdContent;
-            // newQuestion.classList.add("w-100");
-            // newQuestion.classList.add("border", "border-0", "border-bottom", "p-2", "bg-body-tertiary");
+            newQuestionContent.innerHTML= `<div id="${idNewHeaderQuestion}" class="d-flex justify-content-between centrado-vertical header-question"><input class="question ${idNewTextQuestion} w-50 border border-0 border-bottom p-2 rounded"><button id="${idNewResponseOption}" type="button" class="btn btn-outline-success">Añadir Opción</button><select id = "${idNewSelectOptionType}" class="form-select w-25 shadow p-3 mb-5 bg-body-tertiary rounded" aria-label="Default select example"></select><button type="button" class="btn-close close-button" aria-label="Close"></button></div>`;
+            
             divIdContentSection.appendChild(newQuestionContent);
+
+
+            let addOptionButton = document.getElementById(idNewResponseOption);
+            addOptionButton.setAttribute("onclick", "addNewOption()");
+            let selectelement = document.getElementById(idNewSelectOptionType);
+            let countOption = 0;
+            optionsList.forEach( option => {
+                countOption++;
+                let newResponseOption = document.createElement("option");
+
+                newResponseOption.value = option;
+                newResponseOption.textContent = option;
+                if (countOption === 1) {
+                    newResponseOption.selected = true;
+                }
+                selectelement.appendChild(newResponseOption);
+                
+                let selectElement = document.getElementById(idNewSelectOptionType);
+                selectElement.setAttribute("onchange", "changeTypeOption()")
+            })
         }
         
     }
@@ -90,6 +101,72 @@ function createQuestion() {
     document.body.addEventListener("click", myCustomListener);  
     
     
+}
+
+function addNewOption() {
+    let  count = 1;
+    const myCustomListener = function(e) {
+        e.stopPropagation();
+        count++;
+        if (count==2) {
+            let finalPartId = e.target.id.replace("option-", "");
+            let divContentQuestionId = "content-" + finalPartId;
+            let divContentQuestion = document.getElementById(divContentQuestionId);
+            let lengthDivContentQuestionChildren = divContentQuestion.children.length;
+            let numCreatedOptions = lengthDivContentQuestionChildren - 1;
+            let newDivOption = document.createElement("div");
+            let idNewDivOption = "divOption-" + `${numCreatedOptions++}-` + finalPartId;
+            newDivOption.classList.add("d-flex", "justify-content-start", "gap-3", "w-100", "centrado-vertical");
+            newDivOption.id = idNewDivOption;
+            if (lengthDivContentQuestionChildren == 1) {
+                let idSelectParent = "select-" + finalPartId;
+                let selectParent = document.getElementById(idSelectParent);
+                let selectOptionResponseValue = selectParent.value;
+                
+                if (selectOptionResponseValue === "Única Respuesta") {
+                    
+                    newDivOption.innerHTML = `<input type="radio" id="${finalPartId}-${numCreatedOptions++}-unique" class="option-marked p-2 form-check-input border border-primary" name="${finalPartId}" value=""><input type="text" class="border border-0 border-bottom rounded w-25 p-2" id="text-${finalPartId}-${numCreatedOptions++}" value="">`;
+                    
+                } else if (selectOptionResponseValue === "Respuesta Múltiple") {
+
+                    newDivOption.innerHTML = `<input type="checkbox" id="${finalPartId}-${numCreatedOptions++}-several" class="option-marked p-2 form-check-input border border-primary" name="${finalPartId}-${numCreatedOptions++}" value=""><input type="text" class="border border-0 border-bottom rounded w-25 p-2" id="text-${finalPartId}-${numCreatedOptions++}" value="">`;
+
+                } else if (selectOptionResponseValue === "Respuesta Abierta") {
+
+                    newDivOption.innerHTML = `<textarea id="${finalPartId}-text" name="${finalPartId}" rows="3" cols="60" class="rounded p-2 border border-0"></textarea>`;
+
+                }
+
+
+                divContentQuestion.appendChild(newDivOption);
+
+                
+            } else {
+
+                let lastPartIdFirstResponseOptionInto = divContentQuestion.children[1].children[0].id.split("-").slice(-1)[0];
+                
+                if (lastPartIdFirstResponseOptionInto != "text") {
+
+                    if (lastPartIdFirstResponseOptionInto === "unique") {
+
+                        newDivOption.innerHTML = `<input type="radio" id="${finalPartId}-${numCreatedOptions++}-unique" class="option-marked p-2 form-check-input border border-primary" name="${finalPartId}" value=""><input type="text" class="border border-0 border-bottom rounded w-25 p-2" id="text-${finalPartId}-${numCreatedOptions++}" value="">`;
+
+                    } else {
+
+                        newDivOption.innerHTML = `<input type="checkbox" id="${finalPartId}-${numCreatedOptions++}-several" class="option-marked p-2 form-check-input border border-primary" name="${finalPartId}-${numCreatedOptions++}" value=""><input type="text" class="border border-0 border-bottom rounded w-25 p-2" id="text-${finalPartId}-${numCreatedOptions++}" value="">`;
+
+                    }
+
+                    divContentQuestion.appendChild(newDivOption);
+
+                }
+
+            }
+        }
+    }
+
+    document.body.addEventListener("click", myCustomListener);  
+
 }
 
 
@@ -107,6 +184,35 @@ function createChapter() {
     newDiv.innerHTML=`<div id = 'content-${idNewDiv}' class = 'centrado w-75'><input id = 'header-chapter-${numChildren}' type='text' class = 'section-${numChildren} p-3 mb-2 bg-body-secondary text-success-emphasis border border-bottom-0 rounded fw-bold w-100' value = 'Capítulo ${numChildren}'></div><button id = 'but-section-${numChildren}' class = ' but-question section-${numChildren} btn btn-outline-warning ' onclick='createQuestion()'>Nueva pregunta</button>`;
     divContent.appendChild(newDiv);
     
+}
+
+function changeTypeOption() {
+    let count = 1;
+    const myCustomListener = function(e) {
+
+        let finalPartId = e.target.id.replace("select-", "");
+        let idSelectTypeOptionChanged = e.target.id;
+        let selectChangedElement = document.getElementById(idSelectTypeOptionChanged); 
+        let divContentQuestionId = "content-" + finalPartId;
+        let divContentQuestion = document.getElementById(divContentQuestionId);
+        //console.log(divContentQuestion.children.length);
+        
+        // let listObjectIntoDivContentQuestion = divContentQuestion.children;
+        // console.log(listObjectIntoDivContentQuestion);
+
+        if (selectChangedElement.value === "Única Respuesta") {
+
+            // for (div in listObjectIntoDivContentQuestion) {
+            //     console.log(div);
+            // }
+
+        }
+
+    }
+
+    document.body.addEventListener("click", myCustomListener);  
+
+
 }
 
 function sendHTML() {
